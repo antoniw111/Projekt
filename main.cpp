f765d4aa2f5fee41775eff33466f6193380b4713
antoniw111 <antoniw1234@gmail.com>
2023-12-15 19:58:18 +0100

    Dodano funkcje częściowo rysującą plansze do gry

--- main.cpp
+++ main.cpp
@@ -2,20 +2,27 @@
 #include <stdio.h>
 #include <math.h>
 #include "winbgi2.h"
-void ramka();
+void displayBoard();
 int main()
 {
 	graphics(1000, 800);
-	ramka();
+	displayBoard();
 	
 	wait();
 	return 0;
 }
-void ramka()
+void displayBoard()
 {
-	line(5, 5, 990, 5);
+	//obramowanie
+	/*line(5, 5, 990, 5);
 	line(5, 5, 5, 780);
 	line(990, 5, 990, 780);
-	line(5, 780, 990, 780);
-	line((double)(990 - 5) / 2, 5, (double)(990 - 5) / 2, 780);//podzia³ka na graczy
+	line(5, 780, 990, 780);*/
+	//powy¿sze instrukcje zast¹pi³em rectangle'm
+	rectangle(5, 5, 990, 780);
+	//podzia³ka na graczy
+	line((double)(990 - 5) / 2, 5, (double)(990 - 5) / 2, 780);
+	line(5, (double)2*(780 - 5)/3, 990, (double)2*(780 - 5)/3);
+	//plansza gracza
+	rectangle(30, 30, ((990-5)/2)-30, (2 * (780 - 5) / 3)-30);
 }
